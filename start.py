30c6750bd312f169295aca46e8e9e9ce63177999
ry44an <73395340+CODERC755@users.noreply.github.com>
2022-06-18 17:13:43 -0400

    Ping Pong

---
+++ start.py
@@ -0,0 +1,15 @@
+print('''
+MIT prohibits the use of copyrighted materials in a manner that is inconsistent with the U.S. copyright law. MIT policy requires that members of the MIT community not share copyrighted material over the campus network in any way that violates the law. Sharing over the network includes but is not limited to sharing via web pages, peer-to-peer le sharing software, and email. Members of the community should either have the rights or authorization from the copyright holder for any material or determine that fair use applies before it is made available or shared over the campus network. Violations of this policy could result in disciplinary action. Students should also be aware that unauthorized distribution of copyrighted material, including peer-to-peer le sharing, may lead to civil and criminal liabilities.
+
+MIT’s responsibility under the law is to respond expeditiously to remove, or disable access to, the material that is claimed to be infringing. When MIT receives a Digital Millennium Copyright Act (DMCA) notification, it is expeditiously forwarded to the individual member of the MIT community that uses, owns, controls, or has some kind of administrative or technical responsibility for the machine indicated in the notice, when that individual can be identified based on records maintained by Information Systems and Technology (IS&T).
+
+The contacted user is requested to remove or block access to all materials identified as infringing (as well as any other infringing material) and to respond to MIT’s DMCA agent within 5 days of the forwarding of the takedown notice. Know what your responsibilities are if you receive a DMCA notification.
+
+Takedown notice cases are periodically reviewed in order to determine if a possibility of repeat infringement exists. Further action regarding repeat infringement will be coordinated by the Office of Student Conduct and Community Standards. 
+
+For more information, visit Copyright at MIT and MITnet Rules of Use.
+
+''')
+print("Any key to continue.")
+input()
+exec(open("./game/game.py").read())
